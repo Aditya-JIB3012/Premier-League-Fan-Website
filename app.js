dd466fd449e61df1939f8223c46b9a6ad98724b2
Aditya-JIB3012 <adityajibrael@gmail.com>
2026-02-03 02:33:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+// Dropdown toggle
+const dropdownBtn = document.querySelector('.dropbtn');
+const dropdownMenu = document.querySelector('.dropdown-content');
+
+dropdownBtn.addEventListener('click', (e) => {
+  e.stopPropagation(); // prevent click bubbling
+  dropdownMenu.classList.toggle('show');
+});
+
+// Close dropdown if clicking outside
+document.addEventListener('click', (e) => {
+  if (!dropdownMenu.contains(e.target) && !dropdownBtn.contains(e.target)) {
+    dropdownMenu.classList.remove('show');
+  }
+});
